f79dcc833bb288d0fb09117e68a6630bd80f1726
Muhammad Fahril <92075895+Fahril33@users.noreply.github.com>
2025-07-17 03:53:48 +0800

    set Important styles

--- src/page-section/FutureSections.tsx
+++ src/page-section/FutureSections.tsx
@@ -3,33 +3,33 @@ import "../components/styles/futureSection.css";
 import "../components/styles/cards.css";
 function FutureSections() {
     return (
-        <div>
-            <div className="futureSectionContainer" id="future">
-                <h1 className="Text Header">Upcomings</h1>
-                <div className="futureSection">
-                    <div className="card">
-                        <div className="cardContent">
-                            <span className="Title">Planned Project</span>
-                            <div className="contents">
-                                <div className="currentContent">
-                                    <h1>Not Listed.</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="cardContent">
-                            <span className="Title">Planned Learns</span>
-                            <div className="contents">
-                                <div className="currentContent">
-                                    <h1>Not Listed.</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+      <div>
+        <div className="futureSectionContainer" id="future">
+          <h1 className="Text Header">Upcomings</h1>
+          <div className="futureSection">
+            <div className="card">
+              <div className="cardContents">
+                <span className="Title">Planned Project</span>
+                <div className="cardContent">
+                  <div className="currentContent">
+                    <h1>Not Listed.</h1>
+                  </div>
                 </div>
+              </div>
             </div>
+            <div className="card">
+              <div className="cardContents">
+                <span className="Title">Planned Learns</span>
+                <div className="cardContent">
+                  <div className="currentContent">
+                    <h1>Not Listed.</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     );
 }
 
